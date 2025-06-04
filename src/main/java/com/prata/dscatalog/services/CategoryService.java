67a166e6445a4a7a43b2a5eeb38350a9c48e2e97
Gabriel Prata <ghprata7245@gmail.com>
2025-06-04 10:10:56 -0300

    Delete Category

--- src/main/java/com/prata/dscatalog/services/CategoryService.java
+++ src/main/java/com/prata/dscatalog/services/CategoryService.java
@@ -3,12 +3,15 @@ package com.prata.dscatalog.services;
 import com.prata.dscatalog.dto.CategoryDTO;
 import com.prata.dscatalog.entities.Category;
 import com.prata.dscatalog.repositories.CategoryRepository;
+import com.prata.dscatalog.services.exceptions.DatabaseException;
 import com.prata.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -51,6 +54,19 @@ public class CategoryService {
         }
         catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
         }
     }
 }
