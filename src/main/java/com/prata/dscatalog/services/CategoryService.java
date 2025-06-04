3788e4681e64340bb46c8e253724a4597fe90a69
Gabriel Prata <ghprata7245@gmail.com>
2025-06-04 09:18:25 -0300

    H2 database

---
+++ src/main/java/com/prata/dscatalog/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.prata.dscatalog.services;
+
+import com.prata.dscatalog.entities.Category;
+import com.prata.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+}
