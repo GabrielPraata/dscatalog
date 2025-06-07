f499796f8422443473737adc90a316860b22f205
GabrielPraata <ghprata7245@gmail.com>
2025-06-06 22:00:28 -0300

    Test ProductRepository

---
+++ src/test/java/com/prata/dscatalog/tests/Factory.java
@@ -0,0 +1,21 @@
+package com.prata.dscatalog.tests;
+
+import com.prata.dscatalog.dto.ProductDTO;
+import com.prata.dscatalog.entities.Category;
+import com.prata.dscatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+
+    public static Product createProduct() {
+        Product product = new Product(1L, "phone", "Good Phone", 800.0, "http://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+        product.getCategories().add(new Category(1L, "Electronics"));
+        return product;
+    }
+
+    public static ProductDTO createProdutDTO() {
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
