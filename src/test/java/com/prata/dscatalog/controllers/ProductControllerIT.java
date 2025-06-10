ba1d06e464060b5d0509ee401631002f81fa4331
GabrielPraata <ghprata7245@gmail.com>
2025-06-10 11:42:27 -0300

    ProductControllerIT Teste de integração delete

--- src/test/java/com/prata/dscatalog/controllers/ProductControllerIT.java
+++ src/test/java/com/prata/dscatalog/controllers/ProductControllerIT.java
@@ -1,4 +1,59 @@
 package com.prata.dscatalog.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prata.dscatalog.repositories.ProductRepository;
+import com.prata.dscatalog.services.ProductService;
+import com.prata.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ProductControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProductService service;
+
+    @Autowired
+    private ProductRepository repository;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void deleteShouldDeleteResourceWhenIdExists() {
+
+        service.delete(existingId);
+
+        Assertions.assertEquals(countTotalProducts - 1, repository.count());
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.delete(nonExistingId);
+        });
+    }
 }
