3bcde50e6ca329708412449d83c525854c5a0125
GabrielPraata <ghprata7245@gmail.com>
2025-06-11 10:59:54 -0300

    Custom ConstraintValidator

--- src/main/java/com/prata/dscatalog/dto/UserInsertDTO.java
+++ src/main/java/com/prata/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.prata.dscatalog.dto;
 
+import com.prata.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
     private String password;
